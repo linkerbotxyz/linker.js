410a8b54e58c0c1b50f13209e5e406161e7c903f
lasse <77295879+lassv@users.noreply.github.com>
2022-11-14 11:29:55 +0000

    Addded getUserLinks

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 const { clientFetch } = require("../index");
 
-const linkData = new clientFetch("githdub");
+const linkData = new clientFetch("github");
+const userData = new clientFetch("508700048452878347");
 
 linkData.getLink().then((data) => console.log(data));
+userData.getUserLinks().then((data) => console.log(data));
